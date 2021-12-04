67aba843d83a84910653adb40c54af0ee5888d0b
Werner Schneider <werner@schneidernet.com>
2021-12-04 20:52:02 +0100

    ohne Parameter

--- src/date.ts
+++ src/date.ts
@@ -1,12 +1,12 @@
 import { format } from 'date-fns'
 import { de } from 'date-fns/locale'
 
-function formatDay(d: Date) {
-  return format(d, 'P', { locale: de })
+function formatDay(d?: Date) {
+  return format(d || new Date(), 'P', { locale: de })
 }
 
-function formatDayTime(d: Date) {
-  return format(d, 'Pp', { locale: de })
+function formatDayTime(d?: Date) {
+  return format(d || new Date(), 'Pp', { locale: de })
 }
 
 export { formatDay, formatDayTime }
