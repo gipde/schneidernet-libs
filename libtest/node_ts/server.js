1a35f03cb3974cee56d7aed23e968eaa131d5e9b
Werner Schneider <werner@schneidernet.com>
2021-12-09 00:55:22 +0100

    backup

--- libtest/node_ts/server.js
+++ libtest/node_ts/server.js
@@ -1,8 +1,8 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tools_1 = require("@schneidernet/tools");
-console.log('Test');
-tools_1.log.debug('debug');
-tools_1.log.warn('warn');
-tools_1.log.info((0, tools_1.formatDayTime)(new Date()));
-console.log((0, tools_1.formatDay)(new Date()));
+'use strict'
+Object.defineProperty(exports, '__esModule', { value: true })
+const tools_1 = require('@schneidernet/tools')
+console.log('Test')
+tools_1.log.debug('debug')
+tools_1.log.warn('warn')
+tools_1.log.info((0, tools_1.formatDayTime)(new Date()))
+console.log((0, tools_1.formatDay)(new Date()))
