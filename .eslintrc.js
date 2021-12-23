4b077337910ef4f61b394aac410b8c520ac5a6ea
Werner Schneider <werner@schneidernet.com>
2021-12-23 20:03:49 +0100

    config

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  env: {
+    'jest/globals': true,
+  },
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript',
+    'eslint:recommended',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended', // Make sure this is always the last element in the array.
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 13,
+  },
+  plugins: ['simple-import-sort', 'prettier', 'jest'],
+  rules: { 'react/jsx-filename-extension': 'off', 'import/prefer-default-export': 'off' },
+}
