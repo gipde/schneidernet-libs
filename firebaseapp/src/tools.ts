07d0e982220b8b13ef2ca68fb03931be3d8cb7d7
Werner Schneider <werner@schneidernet.com>
2022-11-29 22:12:02 +0100

    react effects fix

--- firebaseapp/src/tools.ts
+++ firebaseapp/src/tools.ts
@@ -1,27 +1,14 @@
 import { updateProfile } from '@firebase/auth'
 import { log } from '@schneidernet/tools'
-import { useSetRecoilState } from 'recoil'
-
 import { auth } from './initFirebase'
-import { uiUserAtom } from './model/ui'
 
 // TODO: hier sollte das mit Recoil State irgendwie funktionieren
 //       aber das funktioniert nur innerhalb einer Component
 //       beim LogOut sollte gleichzeigit der User auf undefined gesetzt werden.
 
 export function setFirebaseUser(displayName: string) {
-  const setUser = useSetRecoilState(uiUserAtom)
   if (auth.currentUser) {
     updateProfile(auth.currentUser, { displayName })
-    setUser((oldUser) => {
-      if (oldUser) {
-        return { ...oldUser, displayName }
-      }
-      log.error(
-        'uiUser nicht gesetzt. Deshalb kann der DisplayName nicht gesetzt werden'
-      )
-      return undefined
-    })
   } else {
     log.error('Kein Benutzer angemeldet')
   }
